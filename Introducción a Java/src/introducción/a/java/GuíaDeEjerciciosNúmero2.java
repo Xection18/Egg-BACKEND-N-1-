cb0c2434f23c2d8a4f387d2c14275ccc7ac6184f
Johann G. Adam <Xection_08@hotmail.com>
2023-05-01 23:14:40 -0300

    Commit #2

--- Introducción a Java/src/introducción/a/java/GuíaDeEjerciciosNúmero2.java
+++ Introducción a Java/src/introducción/a/java/GuíaDeEjerciciosNúmero2.java
@@ -5,6 +5,8 @@
  */
 package introducción.a.java;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Xecti
@@ -15,7 +17,24 @@ public class GuíaDeEjerciciosNúmero2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        1. Escribir un programa que pida dos números enteros por teclado y 
+        calcule la suma de los dos. El programa deberá después mostrar 
+        el resultado de la suma
+        */
+        
+        int num1;
+        int num2;
+        int suma;
+        System.out.println("Deberá introducir dos números de tipo entero para realizar una operación aritmética");
+        System.out.println("Introduzca el primer número: ");
+        Scanner entrada = new Scanner (System.in);
+        num1 = entrada.nextInt();
+        System.out.println("Ok, ahora debera introducir el segundo número");
+        System.out.println("Introduzca el segundo número");
+        num2 = entrada.nextInt();
+        suma = num1 + num2;
+        System.out.println("La suma de los dos números será de: " + suma);
     }
     
 }
