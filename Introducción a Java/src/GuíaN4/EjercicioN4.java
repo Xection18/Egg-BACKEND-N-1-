668aee55cf3a0a03407c257c97251419c93c7555
Johann G. Adam <Xection_08@hotmail.com>
2023-05-08 15:40:43 -0300

    Commit #17

--- Introducción a Java/src/GuíaN4/EjercicioN4.java
+++ Introducción a Java/src/GuíaN4/EjercicioN4.java
@@ -33,7 +33,7 @@ public class EjercicioN4 {
         entre 1 y 100, ellos son: 2, 3, 5, 7, 11, 13, 17,
         19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
         71, 73, 79, 83, 89 y 97.
-        
+ 
         */
         Scanner leer = new Scanner (System.in);
         int numero = 0;
